5cd2b0ca1c6324c09f993a98b55861ca13d37961
John Brilla <jbrilla17@gmail.com>
2016-02-20 11:35:24 +1100

    init commit

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine( program.OutputMessage("This is a message!") );
+        }
+
+        public string OutputMessage(string message)
+        {
+            return message;
+        }
+
+        public string ConcatString(string strOne, string strTwo)
+        {
+            return strOne + strTwo;
+        }
+    }
+}
