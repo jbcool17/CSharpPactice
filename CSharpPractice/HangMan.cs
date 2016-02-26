8ad2497abd3d9eca191229cf22b16efbd8366f2e
John Brilla <john@syd.tempurer.com>
2016-02-26 14:12:52 +1100

    'hangmaning'

--- CSharpPractice/HangMan.cs
+++ CSharpPractice/HangMan.cs
@@ -10,20 +10,20 @@ namespace CSharpPractice
     {
         public string word;
         private static List<string> wordList = new List<string>() { "hello", "world", "dogs", "cats", "fish" };
-        public int score = wordList.Count() ;
+        public int score;
+        public List<char> guessedLetters = new List<char>();
         
+        public string blankWord;
         
-        public HangMan()
+        //Initialize the game 
+        public void init()
         {
-            word = "unknown";
+            word = GetRandWord();
+            blankWord = CreateBlankWord(word);
+            score = 0;
         }
-
-        public void SetName(string newName)
-        {
-            word = newName;
-        }
-
-        public void GetWords()
+                
+        public void GetWordsList()
         {
             foreach (string word in wordList)
             {
@@ -31,12 +31,68 @@ namespace CSharpPractice
             }
         }
 
-        public string GetRandWord()
+        private string GetRandWord()
         {
             Random r = new Random();
             int number = r.Next(0, 4);
+            word = wordList[number];
 
             return wordList[number];
         }
+
+        private string CreateBlankWord(string word)
+        {
+            
+            List<char> blankSpacePush = new List<char>();
+            char blankSpace = '_';
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                blankSpacePush.Add(blankSpace);
+            }
+
+            return string.Join("", blankSpacePush.ToArray());
+        }
+
+        public void checkForLetter(string command)
+        {
+            char[] array = word.ToCharArray();
+            char[] guess = command.ToCharArray();
+            guessedLetters.Add(guess[0]);
+                        
+            Console.WriteLine("Guessed Letter: " + string.Join(",", guessedLetters.ToArray()));
+
+            addsCorrectLetter(guess[0]);
+        }
+
+        internal void checkForWinner()
+        {
+            if ( blankWord == word)
+            {
+                Console.WriteLine("You won!");
+            } else
+            {
+                Console.WriteLine("Sorry please try again");
+            }
+        }
+
+        private void addsCorrectLetter(char letter)
+        {
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                if ( word[i] == letter )
+                {
+                    blankWord = blankWord.Remove(i, 1);
+                    blankWord = blankWord.Insert(i, letter.ToString());
+                    score += 1;
+                    Console.WriteLine("FOUND: " + blankWord);
+                }                   
+               
+            }
+
+            
+        }
+                
     }
 }
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,66 +14,63 @@ namespace CSharpPractice
         {
             Program program = new Program();
             HangMan gameOne = new HangMan();
+                
 
-            //Console.WriteLine( program.OutputMessage("This is a message!") );
+            //Init Game
+            gameOne.init();
+            string randomWord = gameOne.word;
+            string input;
 
-            //Calc calc = new Calc();
-            //Console.WriteLine( calc.AddNum(100, 99) );
-            //Console.WriteLine( calc.SubNum(5, 10) );
+            //Opening Message
+            Console.WriteLine("Welcome to HangMan");
+            Console.WriteLine("Please guess a letter");
+            
+            Console.WriteLine("The Word: " + gameOne.blankWord);
+                       
 
-            //Person personOne = new Person();
-            //Console.WriteLine(personOne.name);
-
-            //Person personTwo = new Person();
-            //personTwo.SetName("JB Cool");
-            //Console.WriteLine(personTwo.name);
-            //Console.WriteLine(personTwo.GetRandName());
-
-            string randWord = gameOne.GetRandWord();
-            string word = "default";
-            int score = gameOne.score;
-
-            Console.WriteLine("Guess a word from this list: ");
-            while ( randWord != word)
+            //Game Loop
+            int guesses = 10;
+            while (guesses > 0)
             {
-                
-                Console.WriteLine("===========");
-                gameOne.GetWords();
-                Console.WriteLine("===========");
-                word = Console.ReadLine();
-                if (word == randWord)
-                {
-                    program.OutputMessage("It matches!");
-                }
-                else if ( score < 3)
-                {
-                    
-                    Console.WriteLine("-----------");
-                    program.OutputMessage("Sorry. You Loose :-( :: Score: " + score);
-                    program.OutputMessage("The word was: " + randWord);
-                    randWord = word;
+                input = Console.ReadLine();
+                gameOne.checkForLetter(input);
+                gameOne.checkForWinner();
+                     
+                guesses -= 1;
 
-                }
-                else 
-                {
-                    score -= 1;
-                    Console.WriteLine("-----------");
-                    program.OutputMessage("Sorry. Please choose again :-( :: Score: " + score);
-                }
-                
-                
+                Console.WriteLine( "Guesses Left: " + guesses);
+                Console.WriteLine( "Score: " + gameOne.score);
             }
-           
-                        
-           // program.OutputMessage(randWord);
-
-
-
             
+            //while ( randWord != word)
+            //{
                 
-            
-            
+            //    Console.WriteLine("===========");
+            //    gameOne.GetWords();
+            //    Console.WriteLine("===========");
+            //    word = Console.ReadLine();
 
+            //    if (word == randWord)
+            //    {
+            //        program.OutputMessage("It matches!");
+            //    }
+            //    else if ( score < 3)
+            //    {
+                    
+            //        Console.WriteLine("-----------");
+            //        program.OutputMessage("Sorry. You Loose :-( :: Score: " + score);
+            //        program.OutputMessage("The word was: " + randWord);
+            //        randWord = word;
+
+            //    }
+            //    else 
+            //    {
+            //        score -= 1;
+            //        Console.WriteLine("-----------");
+            //        program.OutputMessage("Sorry. Please choose again :-( :: Score: " + score);
+            //    }
+                
+            //}
 
         }
 
